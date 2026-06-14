3113ade19e4426d54349fe7a4571df69db03c9ef
watanaben53-cmd <watanabe.n53@gmail.com>
2026-06-15 07:31:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
-const CACHE = 'sgmt-kakaku-v1';
-const FILES = ['/shigematsu-kakaku/', '/shigematsu-kakaku/index.html'];
+const CACHE = 'sgmt-kakaku-v2';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-180.png'
+      ]).catch(function() {
+        return cache.add('./index.html');
+      });
+    })
+  );
   self.skipWaiting();
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    }).then(function(){ return clients.claim(); })
+  );
 });
-self.addEventListener('fetch', e => {
+
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/shigematsu-kakaku/')))
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
   );
 });
